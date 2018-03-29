34b6cb9b616ae3ffe81be53ab0f5ab4bd91febb1
tyf <512451756@qq.com>
2018-03-29 16:59:05 +0800

    d

--- base-test/src/main/java/com/tyf/controller/TestController.java
+++ base-test/src/main/java/com/tyf/controller/TestController.java
@@ -1,20 +1,35 @@
 package com.tyf.controller;
 
+import com.tyf.pojo.User;
+import com.tyf.utils.SSOcheck;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @RequestMapping("/a")
 @Controller
 public class TestController {
-    @RequestMapping("/a")
-    @ResponseBody
-    public String hello(){
-        return  "https://blog.csdn.net/lxn39830435731415926/article/details/46434241";
+    @RequestMapping("")
+    public String hello(ModelMap map){
+        map.addAttribute("host", "http://blog.didispace.com");
+        return "index.html";
     }
 
     @RequestMapping("/b")
-    public String hellso(){
+    @ResponseBody
+    public String hellso(User user, HttpServletResponse response){
+        boolean istrue=SSOcheck.check(user.getAccount(),user.getPass());
+        if(istrue){
+           Cookie coo=new Cookie("sso","sso");
+            coo.setPath("/");
+            response.addCookie(coo);
+            return "success";
+        }
+        System.out.println(user.getAccount()+"===========================");
         return "hello.html";
     }
 
